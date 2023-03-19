9afd4c33359e61101729c704ede05f9b8ce36808
Shahithya Sukumar <shahithya0306@gmail.com>
2023-03-19 11:22:27 -0400

    Added Cors for Backend Deployment

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
 
 connectDB();
 
@@ -12,22 +14,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}))
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
-
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
 
 app.use(errorHandler);
 
