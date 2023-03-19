03c1af521167394b2cff297d237935f61ccb1c30
Shahithya Sukumar <shahithya0306@gmail.com>
2023-03-19 05:04:21 -0400

    Revert "Added Cors"
    This reverts commit e421f363cb07b834f412d93f4a6915149bc3cb32.

--- config/allowedOrigins.js
+++
@@ -1,6 +0,0 @@
-const allowedOrigins = [
-    // 'http://localhost:3000',
-    'https://goalsetting.onrender.com'
-]
-
-module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const cors = require('cors')
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const corsOptions = require("./config/corsOptions");
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
-app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +16,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
 
